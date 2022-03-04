56baebaa3f173b52c59fcf35273f31bc3701fa66
Sergey Vasilyev <nolar@nolar.info>
2022-03-04 12:51:01 +0100

    Enable bash history navigation backwards (CtrlS)

--- .bash_profile.d/history.sh
+++ .bash_profile.d/history.sh
@@ -10,6 +10,9 @@ export HISTSIZE="99999"
 # Append-clear-reload — to keep the history synced across sessions and tabs.
 export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND;} history -a; history -c; history -r"
 
+# Disable Ctrl-S session freezing, to enable the history navigation with CtrlR/CtrlS.
+stty -ixon
+
 update_terminal_cwd() {
     true
 }
